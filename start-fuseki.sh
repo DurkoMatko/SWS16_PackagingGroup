38ddbf0549a1ee7ca622527522225f66e498c98d
Patrik <patrik@keller-re.de>
2016-06-15 20:23:29 +0200

    Avoid .swsrc in start-fuseki.sh

--- start-fuseki.sh
+++ start-fuseki.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 
-# load environment
-source ~/.swsrc
+### Configure your environment here
 
+PROJECT_HOME=$PWD
+FUSEKI_HOME=$HOME/opt/apache-jena-fuseki-2.3.1
 
-### CONFIGURATION
+### Other configuration
 
 tmpdir=/tmp
 port=3030
-ds=ds
 cfgfile=$PROJECT_HOME/etc/fuseki-config.ttl
+
+### Do stuff
+
 host=http://localhost:$port
-
-### Script
-
+ds=ds # This depends on the fuseki-config.ttl
 wrkdir=$tmpdir/swsfuseki
-# enforce clean start
+
+# Enforce clean start
 rm -rf $wrkdir
 mkdir $wrkdir
 
 (
 
-#wait for server
+# Wait for fuseki
+
 sleep 20;
 
-# beautify data by iterating over corresponding folder
+# Beautify data by iterating over corresponding folder
+
 for sparql in $PROJECT_HOME/sparql/init/*.sparql
 do
   echo "# run $sparql"
-  s-update --service $host/$ds/update --file $sparql
+  $FUSEKI_HOME/bin/s-update --service $host/$ds/update --file $sparql
 done
 
 ) &
 
-# start fuseki in mem
-(cd $wrkdir; $FUSEKI_HOME/fuseki-server --localhost --config $cfgfile)
+# Start fuseki in mem
+(cd $wrkdir; $FUSEKI_HOME/fuseki-server --port $port --localhost --config $cfgfile)
 
