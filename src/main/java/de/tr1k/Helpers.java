a0c88d1035a46033a66d5be33b785bb3d3fb45b2
nitramdurcek <nitramdurcek@gmail.com>
2016-06-17 22:27:27 +0200

    events endpoint

--- src/main/java/de/tr1k/Helpers.java
+++ src/main/java/de/tr1k/Helpers.java
@@ -32,5 +32,42 @@ public class Helpers{
 		model.write(outputStream, "JSON-LD");
 		return outputStream;
 	}
+	
+	 public static String queryFromInnerSelect(String select){
+		    String query = ""
+		      + "PREFIX schema: <http://schema.org/>\n"
+		      + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n"
+		      + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+		      + "PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>"
+		      + "CONSTRUCT {\n"
+		      + "  ?s ?p ?o.\n"
+		      + "  ?img ?imgp ?imgo.\n"
+		      + "  ?address ?addressp ?addresso.\n"
+		      + "  ?country ?countryp ?countryo.\n"
+		      + "  ?price ?pricep ?priceo.\n"
+		      + "  ?geo ?geop ?geoo.\n"
+		      //+ "  ?offer ?offerp ?offero.\n"
+		      //+ "  ?price2 ?price2p ?price2o.\n"
+		      //+ "  ?image2 ?image2p ?image2o.\n"
+		      + "}\n"
+		      + "WHERE {\n"
+		      + "  {\n"
+		      +      select
+		      + "  }\n"
+		      + "  ?s ?p ?o.\n"
+		      + "  OPTIONAL { ?s schema:priceSpecification ?price. ?price ?pricep ?priceo. }\n"
+		      + "  OPTIONAL { ?s schema:address ?address. ?address ?addressp ?addresso. }\n"
+		      + "  OPTIONAL { ?s schema:address/schema:addressCountry ?country. ?country ?countryp ?countryo. }\n"
+		      + "  OPTIONAL { ?s schema:image ?img. ?img ?imgp ?imgo. }\n"
+		      + "  OPTIONAL { ?s schema:geo ?geo. ?geo ?geop ?geoo. }\n"
+		      // The makesOffer recursion makes my laptop explode
+		      //+ "  OPTIONAL { ?s schema:makesOffer ?offer. ?offer ?offerp ?offero. }\n"
+		      //+ "  OPTIONAL { ?s schema:makesOffer/schema:priceSpecification ?price2. ?price2 ?price2p ?price2o. }\n"
+		      //+ "  OPTIONAL { ?s schema:makesOffer/schema:image ?image2. ?image2 ?image2p ?image2o. }\n"
+		      + "}\n"
+		      ;
+
+		    return query;
+		  }
 
 }
